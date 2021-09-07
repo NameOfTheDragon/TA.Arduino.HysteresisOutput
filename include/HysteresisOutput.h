a856bb0fdd28500da4f74b3be6e067c42fcd958f
Tim Long <Tim@tigranetworks.co.uk>
2021-09-07 23:46:56 +0100

    Initial import

---
+++ include/HysteresisOutput.h
@@ -0,0 +1,57 @@
+/*
+ Name:		HysteresisOutput.h
+ Author:	Tim Long (TimTek)
+*/
+
+#pragma once
+#include <Timer.h>
+#include <functional>
+
+/// <summary>
+/// Class HysteresisOutput.
+/// Accepts an input state and produces an output action which guarantees minimum stable on and off times.
+/// That is, once the output changes state, that state is latched for a minimum period.
+/// Example application: Given a rain detector that generates a pulse whenever a raindrop is detected, we
+/// might wish to produce an output that is latched on for several minutes from the last rain drop detection.
+/// </summary>
+class HysteresisOutput
+	{
+	public:
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HysteresisOutput"/> class.
+		/// </summary>
+		/// <param name="MinimumTimeOn">The minimum time that the output can be on before turning off.</param>
+		/// <param name="MinimumTimeOff">The minimum time that the output can be off before turning on.</param>
+		/// <param name="stateChanger">An action method that will be called to change the output state.</param>
+		HysteresisOutput(Duration MinimumTimeOn, Duration MinimumTimeOff, std::function<void(bool)> stateChanger)
+			: minimumTimeOn(MinimumTimeOn), minimumTimeOff(MinimumTimeOff), invokeUserStateChange(stateChanger) { }
+
+		/// <summary>
+		/// Turns the output on. May be delayed by hysteresis.
+		/// </summary>
+		void turnOn();
+
+		/// <summary>
+		/// Turns the output off. May be delayed by hysteresis
+		/// </summary>
+		void turnOff();
+
+		//void forceOn();
+		//void forceOff();
+		/// <summary>
+		/// Must be called periodically, typically from the Arduino main loop.
+		/// Timing resolution is dictated by how frequently this method is called, but is 1 ms at best.
+		/// </summary>
+		void loop();
+
+	private:
+		void setOutputState(bool state);
+		Duration getHysteresisTime();
+		void setInputState(bool requestedState);
+		Duration minimumTimeOn;
+		Duration minimumTimeOff;
+		std::function<void(bool)> invokeUserStateChange;
+		bool targetState;
+		Timer hysteresis;
+		bool currentState;
+	};
---
+++ src/HysteresisOutput.cpp
@@ -0,0 +1,69 @@
+/*
+ Name:		HysteresisOutput.cpp
+ Author:	Tim Long (TimTek)
+ Editor:	http://www.visualmicro.com
+*/
+
+#include <functional>
+#include "HysteresisOutput.h"
+
+void HysteresisOutput::turnOn()
+{
+	setInputState(true);
+}
+
+void HysteresisOutput::turnOff()
+{
+	setInputState(false);
+}
+
+void HysteresisOutput::loop()
+{
+	// We don't need to take any action if the current state is already equal to the target state.
+	if (currentState == targetState)
+		return;
+	// There is a pending state change, but we must wait for the hysteresis timer to expire.
+	if (!hysteresis.expired())
+		return;
+	// Now we have a peddling state change and an expired timer: we can change the output state.
+	setOutputState(targetState);
+}
+
+/// <summary>
+/// Sets the output state immediately and starts the hysteresis timer.
+/// Since by definition we can only change output state if no hysteresis timer
+/// is running, then we must also start the hysteresis timer so that the state is latched.
+/// </summary>
+/// <param name="state">The new output state.</param>
+void HysteresisOutput::setOutputState(bool state)
+{
+	currentState = state;
+	invokeUserStateChange(state);
+	hysteresis.setDuration(getHysteresisTime());
+}
+
+Duration HysteresisOutput::getHysteresisTime() { return currentState ? minimumTimeOn : minimumTimeOff; }
+
+/// <summary>
+/// Sets the input state. This may or may not result in the output state changing.
+/// </summary>
+/// <param name="requestedState">The state being requested.</param>
+void HysteresisOutput::setInputState(bool requestedState)
+{
+	const bool returningToCurrentState = targetState != currentState && requestedState == currentState;
+	targetState = requestedState;
+
+	// No hysteresis delay, immediate change in output state required.
+	if (!hysteresis.enabled())
+	{
+		setOutputState(requestedState);
+		return;
+	}
+
+	// A hysteresis timer is running and state change must be deferred
+	// However if we've changed away and back to the current state, the timer gets restarted.
+	if (returningToCurrentState)
+	{
+		hysteresis.setDuration(getHysteresisTime());
+	}
+}
